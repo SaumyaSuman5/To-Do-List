9bf7192617ee3e1afca5e48ebf52dc4d0a2f4dfb
Saumya <saumyasuman539@gmail.com>
2021-09-10 23:43:16 +0530

    Updates

---
+++ precache-manifest.d59c66981dbd86eb42d214e65b462520.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67195a0b59f3c65d0d1a34eb0951b4a",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "97d2342d8bd0ff1f5482",
+    "url": "/list/static/js/2.dc837827.chunk.js"
+  },
+  {
+    "revision": "9fc74c5ebe8631e6bf34",
+    "url": "/list/static/js/main.8d9fd4e4.chunk.js"
+  },
+  {
+    "revision": "25e9e18caae24032992a",
+    "url": "/list/static/js/runtime-main.b000cb72.js"
+  }
+]);
